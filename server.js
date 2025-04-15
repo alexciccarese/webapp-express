cfe127fcf15d758b7a78418713d43a21072af0f2
alexciccarese <alex.ciccarese@gmail.com>
2025-04-15 15:39:16 +0200

    feat: initialize Express web application with basic setup
    - Added package.json with dependencies: express, cors, and mysql2
    - Created server.js to set up Express server with CORS and JSON middleware
    - Implemented basic routes and error handling for 500 and 404 errors

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const PORT = process.env.port || 3004
+
+//middleware
+app.use(cors(
+  {
+    origin: process.env.FRONT_URL || 'htthttp://localhost:5173'
+  }
+))
+
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+
+})
+
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Movies API server')
+})
+
+
+
+//500 server error
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+  
+})
+
+//404 error
+app.use((req, res, next) => {
+  res.status(404).send('Sorry, route does not exist')
+})
