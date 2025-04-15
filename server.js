a5322a7bc1f99af1becd5bb4a7889abcd5af47c0
alexciccarese <alex.ciccarese@gmail.com>
2025-04-15 15:45:24 +0200

    feat: add movie listing and retrieval endpoints

--- server.js
+++ server.js
@@ -28,6 +28,20 @@ app.get('/', (req, res) => {
 })
 
 
+//index 
+app.get('/', (req, res) => {
+
+  res.json({ message: 'List of movies' })
+})
+
+//show
+app.get('/:id', (req, res) => {
+
+  const { id } = req.params
+  res.json({ message: `List of movies by id:${id}` })
+})
+
+
 
 //500 server error
 app.use((err, req, res, next) => {
