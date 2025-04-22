f74e604ea3eddb327e6fd82dcfe4a452ddfbd3b2
alexciccarese <alex.ciccarese@gmail.com>
2025-04-22 21:34:34 +0200

    fix: correct condition to check for empty movie results in show function

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -18,7 +18,7 @@ function show(req, res) {
 
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ error: err.message })
-    if (results === 0) return res.status(404).json({ error: 'Movie not found' })
+    if (results.length === 0) return res.status(404).json({ error: 'Movie not found' })
 
     const movie = results[0]
 
